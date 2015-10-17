c773e8012a90a4ed17898c462c30f51812ecd80a
玄清介 <dupontjoy@163.com>
2015-10-17 19:48:44 +0800

    10月17日

--- 重新部署.bat
+++ 重新部署.bat
@@ -1,2 +1,9 @@
+set outdir=%~dp0
+xcopy /S /Y "%outdir%\data\Rime\cangjie5.custom.yaml" "%appdata%\Google\Google Input Tools\Rime\"
+md %pf%"\Google"
+
 del "%appdata%\Google\Google Input Tools\Rime\default.yaml"
 tskill /v GoogleInputHandler
+
+net stop googleinputservice & tskill GoogleInputHandler
+net start googleinputservice
