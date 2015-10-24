c108b6d3eb2528b15d591b14a5abba4a7329dda6
玄清介 <dupontjoy@163.com>
2015-10-24 13:26:15 +0800

    10.24

---
+++ uninstall.bat
@@ -0,0 +1,16 @@
+set outdir=%~dp0
+if %PROCESSOR_ARCHITECTURE% == AMD64 (
+  set pf="C:\Program Files (x86)"
+) else (
+  set pf="C:\Program Files"
+)
+net stop GoogleInputService
+%pf%"\Google\Google Input Tools\GoogleInputService.exe" /UnregServer
+taskkill /f /im GoogleInputHandler.exe
+%outdir%\win32_register.exe --action=unregister_ime --languageid=0x0804 --name=InputTools --filename=input.ime
+del c:\windows\system32\input.ime
+del c:\windows\syswow64\input.ime
+rmdir /s /q "C:\ProgramData\Google\Google Input Tools"
+::修改
+rmdir /s /q %appdata%"\Google"
+pause
--- 重新部署.bat
+++ 重新部署.bat
@@ -1,7 +1,8 @@
+::复制五仓设置
 set outdir=%~dp0
 xcopy /S /Y "%outdir%\data\Rime\cangjie5.custom.yaml" "%appdata%\Google\Google Input Tools\Rime\"
-md %pf%"\Google"
 
+md %pf%"\Google"
 del "%appdata%\Google\Google Input Tools\Rime\default.yaml"
 tskill /v GoogleInputHandler
 
