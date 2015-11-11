5063dfa391caf5b829cfa62e41b390b3794869bd
玄清介 <dupontjoy@163.com>
2015-11-11 20:10:00 +0800

    11.11

--- beta版/卸载.bat
+++ beta版/卸载.bat
@@ -4,13 +4,24 @@ if %PROCESSOR_ARCHITECTURE% == AMD64 (
 ) else (
   set pf="C:\Program Files"
 )
+
+
 net stop GoogleInputService
+
 %pf%"\Google\Google Input Tools\GoogleInputService.exe" /UnregServer
+
+
+
 taskkill /f /im GoogleInputHandler.exe
+
 %outdir%\win32_register.exe --action=unregister_ime --languageid=0x0804 --name=InputTools --filename=input.ime
+
 del c:\windows\system32\input.ime
 del c:\windows\syswow64\input.ime
+
 rmdir /s /q "C:\ProgramData\Google\Google Input Tools"
-::修改
+
 rmdir /s /q %appdata%"\Google"
+
+
 pause
